47ae0e06e12e5f6b698d50603437e663103ffa41
EdenMD <techn2533@gmail.com>
2026-01-17 06:54:02 +0200

    Update: Ensure test Java file is present.

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+//----start of HelloWorld_java----
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, DEX World!");
+        printMessage();
+    }
+
+    public static void printMessage() {
+        String message = "This is a test Java file to be converted to DEX.";
+        System.out.println(message);
+    }
+}
+//----end of HelloWorld_java----
